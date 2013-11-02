dae846bbd22ca7a58537c91b17440df204394a3c
Rapptz <rapptz@gmail.com>
2013-11-02 03:32:04 -0400

    Added support for --version command line argument

--- shinobi.cpp
+++ shinobi.cpp
@@ -10,6 +10,13 @@ void show_help() noexcept {
     std::cout << std::left << '\t' << std::setw(25) << "-r, --release" << "create release ninja file (default)" << '\n';
 }
 
+void show_version() noexcept {
+    std::cout << "shinobi version " << SHINOBI_VERSION << '\n';
+    std::cout << "Copyright (C) 2013 Rapptz\n"
+                 "This is free software; see the source for copying conditions. There is NO\n"
+                 "warranty; not even for MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.\n";
+}
+
 int main(int argc, char* argv[]) {
     std::string filename("build.ninja");
     std::map<std::string, int> args;
@@ -19,6 +26,11 @@ int main(int argc, char* argv[]) {
 
     if(args.count("-h") || args.count("--help")) {
         show_help();
+        return 0;
+    }
+
+    if(args.count("-v") || args.count("--version")) {
+        show_version();
         return 0;
     }
 
--- util/config.hpp
+++ util/config.hpp
@@ -1,11 +1,10 @@
 #ifndef SHINOBI_UTIL_CONFIG_HPP
 #define SHINOBI_UTIL_CONFIG_HPP
 
-#if !defined(SHINOBI_VERSION) && !defined(SHINOBI_VERSION_MAJOR) && !defined(SHINOBI_VERSION_MINOR)
 #define SHINOBI_VERSION_MAJOR 0
 #define SHINOBI_VERSION_MINOR 9
-#define SHINOBI_VERSION 0.9
-#endif // Versioning
+#define SHINOBI_VERSION_PATCH 1
+#define SHINOBI_VERSION "0.9.1"
 
 #if defined(_WIN32) || defined(__WIN32__)
 #define SHINOBI_WINDOWS
