11570727b988b91a5e377ce16a03ec834cfb2663
Rapptz <rapptz@gmail.com>
2013-11-03 15:57:50 -0500

    Add default shinobi file

--- main.cpp
+++ main.cpp
@@ -1,5 +1,37 @@
 #include <util/shinobi.hpp>
+#include <iostream>
+
+void make_default_shinobi() {
+    std::ofstream out("Shinobi2");
+    out << 
+R"shin({
+    "project": "untitled",
+    "type": "software",
+
+    "compiler": {
+        "name": "g++",
+        "flags": ["-std=c++11", "-pedantic", "-pedantic-errors", "-Wextra", "-Wall", "-O2"]
+    },
+
+    "linker": {
+        "flags": ["-static"]
+    },
+
+    "include_paths": ["."],
+
+    "directory": {
+        "source": "src",
+        "build": "bin",
+        "object": "obj"
+    }
+})shin";
+}
 
 int main() {
-    
+    util::shinobi file;
+
+    if(!file.is_open()) {
+        make_default_shinobi();
+        file.reopen();
+    }
 }
