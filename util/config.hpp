e8db1bb63b31289186f4b029f38b1f9fe4eb05a1
Rapptz <rapptz@gmail.com>
2013-11-06 02:58:49 -0500

    Upgrade version number to 0.9.4, fix OS identification

--- util/config.hpp
+++ util/config.hpp
@@ -3,16 +3,16 @@
 
 #define SHINOBI_VERSION_MAJOR 0
 #define SHINOBI_VERSION_MINOR 9
-#define SHINOBI_VERSION_PATCH 1
-#define SHINOBI_VERSION "0.9.1"
+#define SHINOBI_VERSION_PATCH 4
+#define SHINOBI_VERSION "0.9.4"
 
-#if defined(_WIN32) || defined(__WIN32__)
+#if defined(_WIN32) || defined(__WIN32__) || defined(_WIN64)
 #define SHINOBI_WINDOWS
 
 #elif defined(linux) || defined(__linux)
 #define SHINOBI_LINUX
 
-#elif defined(__APPLE__) || defined(MACOSX) || defined(macintosh) || defined(Macintosh)
+#elif defined(__APPLE__) || defined(__MACH__) || defined(macintosh) || defined(Macintosh)
 #define SHINOBI_MACOS
 
 #endif // System-specific
