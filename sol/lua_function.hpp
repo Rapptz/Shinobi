6c127fe7d4bf029209fffeee83c05a484a28a053
Rapptz <rapptz@gmail.com>
2013-12-13 11:21:10 -0500

    Squashed 'libs/sol/' changes from a4a4d21..8db6783
    8db6783 Add test cases for operator[]
    af0097f Add support for setting functions with operator[]
    7cc8c93 Move type traits to its own header and clean up code using the traits
    5eeaede Basic operator[] support for tables
    d44a215 Remove trivial destructors from lua_func types
    ffb32f4 Renamed sol.scratch.cpp to tests.cpp
    5e83795 Update README
    9f36db8 Rename script_file to open_file
    c241a3d Remove redundant check for narr and nrec variables in create_table
    fdf145e Minor stylistic changes to be consistent
    61e49d9 Merge pull request #6 from ThePhD/master
    a593207 We apparently need to link in `dl` (`dladdr`) manually.
    2b4f9cc Meh, who needs "eval". It's technically right, but I think evaluate might be for shorter lines and stuff... who knows.
    0da6ff4 Test cases for tables and the opening of libraries.
    e9d90d6 Well, -llua5.2. At least it's more explicit.
    9204a01 Checking once more if we need additional library paths...
    029bd91 Weeee, = instead of -!
    b77fd29 ~Installed~
    e6a9688 If this works, Ninja is stupid.
    459bbca GCC is lame. But at least it's building now. Fixed up the build.ninja and made all tests pass (exceptions weren't being thrown because of `lua_pcall`: use `lua_call` to let exceptions propogate naturally).
    bcf4b9b Have I mentioned I hate GCC? I hate GCC.
    350f430 Improved tests, added a build.ninja for TeamCity, and fixed formatting again to work with Rapptz's style.
    dd63621 Added some catch tests and a basic nil getter.
    e7f8ec9 CATCH for tests.
    a5d1458 Adding sol.scratch.cpp, and will also add CATCH for tests.
    fc19896 Some changes to make sure builds go through on 64-bit.
    9e47ec6 Merge branch 'master' of https://github.com/ThePhD/sol into ThePhD
    e53c47f Okay, so now it compiles without errors or warnings and works pretty well. This should be the last piece necessary for full working conformance. Yey~
    404dc64 Merge branch 'master' of https://github.com/ThePhD/sol into ThePhD
    19d01ec Okay, cleaned up includes and now the newest feature: STATEFUL FUNCTIONS! Any stateful function now works and is properly cleaned up, thanks to some additional metatables that are associated with the function values. This lays the ground work for class bindings, but that's a far off dream. For now, table retrieval and `operator[]` is what's for dinner.
    21142e7 Removed std::unordered_map storage on tables since they were getting deleted anyways. Memory leaks are currently present: will have to figure out how to patch those up.
    5996731 Remove construction from a filename as it's pretty useless
    51f7010 Additional `nullptr`'s for GCC's whining ass, and more reinterpret casts. Also for GCC's whiny ass.
    2d2ad59 Well, now that we've optimized l-values, the only reason for explicit_lua_func and member pointers is for it to actually store it. To we store a `T` now, isntead of a `T*`.
    a66ceb9 GCC is retarded. Well, mostly retarded, anyhow...
    288fd13 Ranged for loop. Because ~~clean code~~.
    854cbee Fixing style to fit @Rapptz's usual no-tabs, four-space-indents, template<> no-spaces stuff.
    49c73c4 Heavy optimizations to stateless and lvalue member functions allow us to make many of the general use cases of lambdas and member function pointers work out. This will be useful. The next fix has tobe in table.hpp in the std::false_type ... specialization of the private `set_fx` function.
    6d6dd3d Adding mingw libraries (liblua.a) to gitignore
    8c023c5 This properly pushes a local function to the table. However, we're having issues because the tables are being constructed without an attachment to sol::state, making it impossible to keep std::shared_ptr's alive that contain the virtual interfaces necessary to handle those goddamn lambdas.
    
    git-subtree-dir: libs/sol
    git-subtree-split: 8db67834b2bc55a4e0310eee3e2e35a143fd6821

--- sol/lua_function.hpp
+++ sol/lua_function.hpp
@@ -24,22 +24,156 @@
 
 #include "functional.hpp"
 #include "stack.hpp"
+#include <memory>
 
 namespace sol {
 namespace detail {
+template<typename T, std::size_t N>
+void get_upvalue_ptr(lua_State* L, T*& data, std::size_t datasize, std::array<void*, N> voiddata, int& upvalue) {
+    for (std::size_t i = 0, d = 0; d < datasize; ++i, d += sizeof(void*)) {
+        voiddata[i] = lua_touserdata(L, lua_upvalueindex(upvalue++));
+    }
+    data = reinterpret_cast<T*>(static_cast<void*>(voiddata.data()));
+}
+template<typename T, std::size_t N>
+void get_upvalue_ptr(lua_State* L, T*& data, std::array<void*, N> voiddata, int& upvalue) {
+    get_upvalue_ptr(L, data, sizeof(T), voiddata, upvalue);
+}
+template<typename T>
+void get_upvalue_ptr(lua_State* L, T*& data, int& upvalue) {
+    const static std::size_t data_t_count = (sizeof(T)+(sizeof(void*)-1)) / sizeof(void*);
+    typedef std::array<void*, data_t_count> data_t;
+    data_t voiddata{{}};
+    return get_upvalue_ptr(L, data, voiddata, upvalue);
+}
+template<typename T>
+void get_upvalue(lua_State* L, T& data, int& upvalue) {
+    const static std::size_t data_t_count = (sizeof(T)+(sizeof(void*)-1)) / sizeof(void*);
+    typedef std::array<void*, data_t_count> data_t;
+    data_t voiddata{{}};
+    for (std::size_t i = 0, d = 0; d < sizeof(T); ++i, d += sizeof(void*)) {
+        voiddata[i] = lua_touserdata(L, lua_upvalueindex(upvalue++));
+    }
+    data = *reinterpret_cast<T*>(static_cast<void*>(voiddata.data()));
+}
+} // detail
 
-struct lua_func {
-    virtual int operator()(lua_State*) {
-        throw sol_error("Failure to call specialized wrapped C++ function from lua");
+
+template<typename TFx>
+struct static_lua_func {
+    typedef typename std::remove_pointer<typename std::decay<TFx>::type>::type fx_t;
+    typedef detail::function_traits<fx_t> fx_traits;
+
+    template<typename... Args>
+    static int typed_call(types<void>, types<Args...> t, fx_t* fx, lua_State* L) {
+        stack::pop_call(L, fx, t);
+        return 0;
     }
 
-    virtual ~lua_func() {};
+    template<typename... TRn, typename... Args>
+    static int typed_call(types<TRn...>, types<Args...> t, fx_t* fx, lua_State* L) {
+        auto r = stack::pop_call(L, fx, t);
+        stack::push(L, std::move(r));
+        return sizeof...(TRn);
+    }
+
+    static int call(lua_State* L) {
+        int upvalue = 1;
+        fx_t* fx;
+        detail::get_upvalue(L, fx, upvalue);
+        int r = typed_call(tuple_types<typename fx_traits::return_type>(), typename fx_traits::args_type(), fx, L);
+        return r;
+    }
+
+    int operator()(lua_State* L) {
+        return call(L);
+    }
+};
+
+template<typename T, typename TFx>
+struct static_object_lua_func {
+    typedef typename std::decay<TFx>::type fx_t;
+    typedef detail::function_traits<fx_t> fx_traits;
+
+    template<typename... Args>
+    static int typed_call(types<void>, types<Args...>, T& item, fx_t& ifx, lua_State* L) {
+        auto fx = [&item, &ifx](Args&&... args) { (item.*ifx)(std::forward<Args>(args)...); };
+        stack::pop_call(L, fx, types<Args...>());
+        return 0;
+    }
+
+    template<typename TR, typename... Args>
+    static int typed_call(types<TR>, types<Args...>, T& item, fx_t& ifx, lua_State* L) {
+        auto fx = [&item, &ifx](Args&&... args) -> TR { 
+           return (item.*ifx)(std::forward<Args>(args)...); 
+        };
+        auto r = stack::pop_call(L, fx, types<Args...>());
+        stack::push(L, std::move(r));
+        return 1;
+    }
+
+    template<typename... TRn, typename... Args>
+    static int typed_call(types<TRn...>, types<Args...>, T& item, fx_t& ifx, lua_State* L) {
+        auto fx = [&item, &ifx](Args&&... args) -> std::tuple<TRn...> { return (item.*ifx)(std::forward<Args>(args)...); };
+        auto r = stack::pop_call(L, fx, types<Args...>());
+        stack::push(L, std::move(r));
+        return sizeof...(TRn);
+    }
+
+    static int call(lua_State* L) {
+        const static std::size_t data_t_count = (sizeof(fx_t)+(sizeof(void*)-1)) / sizeof(void*);
+        typedef std::array<void*, data_t_count> data_t;
+        int upvalue = 1;
+        data_t data = {{}};
+        fx_t* fxptr;
+        for (std::size_t i = 0, d = 0; d < sizeof(fx_t*); ++i, d += sizeof(void*)) {
+            data[i] = lua_touserdata(L, lua_upvalueindex(upvalue++));
+        }
+        fxptr = reinterpret_cast<fx_t*>(static_cast<void*>(data.data())); 
+        fx_t& mem_ptr = *fxptr;
+        void* objectdata = lua_touserdata(L, lua_upvalueindex(upvalue++));
+        T& obj = *static_cast<T*>(objectdata);
+        int r = typed_call(tuple_types<typename fx_traits::return_type>(), typename fx_traits::args_type(), obj, mem_ptr, L);
+        return r;
+    }
+
+    int operator()(lua_State* L) {
+        return call(L);
+    }
+};
+
+struct lua_func {
+    static int call(lua_State* L) {
+        void** pinheritancedata = static_cast<void**>(lua_touserdata(L, lua_upvalueindex(1)));
+        void* inheritancedata = *pinheritancedata;
+        if (inheritancedata == nullptr)
+            throw sol_error("call from Lua to C++ function has null data");
+        lua_func* pfx = static_cast<lua_func*>(inheritancedata);
+        lua_func& fx = *pfx;
+        int r = fx(L);
+        return r;
+    }
+
+    static int gc(lua_State* L) {
+        void** puserdata = static_cast<void**>(lua_touserdata(L, 1));
+        void* userdata = *puserdata;
+        lua_func* ptr = static_cast<lua_func*>(userdata);
+        std::default_delete<lua_func> dx{};
+        dx(ptr);
+        return 0;
+    }
+    
+    virtual int operator()(lua_State*) {
+        throw sol_error("failure to call specialized wrapped C++ function from Lua");
+    }
+
+    virtual ~lua_func() {}
 };
 
 template<typename TFx>
 struct lambda_lua_func : public lua_func {
     typedef decltype(&TFx::operator()) fx_t;
-    typedef function_traits<fx_t> fx_traits;
+    typedef detail::function_traits<fx_t> fx_traits;
     TFx fx;
 
     template<typename... FxArgs>
@@ -66,7 +200,7 @@ struct lambda_lua_func : public lua_func {
 template<typename TFx, typename T = TFx, bool is_member_pointer = std::is_member_function_pointer<TFx>::value>
 struct explicit_lua_func : public lua_func {
     typedef typename std::remove_pointer<typename std::decay<TFx>::type>::type fx_t;
-    typedef function_traits<fx_t> fx_traits;
+    typedef detail::function_traits<fx_t> fx_traits;
     TFx fx;
 
     template<typename... FxArgs>
@@ -85,7 +219,7 @@ struct explicit_lua_func : public lua_func {
     template<typename... TRn, typename... Args>
     int operator()(types<TRn...>, types<Args...> t, lua_State* L) {
         auto r = stack::pop_call(L, fx, t);
-        stack::push(L, r);
+        stack::push(L, std::move(r));
         return sizeof...(TRn);
     }
 };
@@ -93,25 +227,22 @@ struct explicit_lua_func : public lua_func {
 template<typename TFx, typename T>
 struct explicit_lua_func<TFx, T, true> : public lua_func {
     typedef typename std::remove_pointer<typename std::decay<TFx>::type>::type fx_t;
-    typedef function_traits<fx_t> fx_traits;
+    typedef detail::function_traits<fx_t> fx_traits;
     struct lambda {
-        T* member;
+        T member;
         TFx invocation;
 
         template<typename... FxArgs>
-        lambda(T* m, FxArgs&&... fxargs): member(m), invocation(std::forward<FxArgs>(fxargs)...) {}
+        lambda(T m, FxArgs&&... fxargs): member(std::move(m)), invocation(std::forward<FxArgs>(fxargs)...) {}
 
         template<typename... Args>
         typename fx_traits::return_type operator()(Args&&... args) {
-           return ((*member).*invocation)(std::forward<Args>(args)...);
+           return (member.*invocation)(std::forward<Args>(args)...);
         }
     } fx;
 
     template<typename... FxArgs>
-    explicit_lua_func(T* m, FxArgs&&... fxargs): fx(m, std::forward<FxArgs>(fxargs)...) {}
-
-    template<typename... FxArgs>
-    explicit_lua_func(T& m, FxArgs&&... fxargs): fx(std::addressof(m), std::forward<FxArgs>(fxargs)...) {}
+    explicit_lua_func(T m, FxArgs&&... fxargs): fx(std::move(m), std::forward<FxArgs>(fxargs)...) {}
 
     virtual int operator()(lua_State* L) override {
         return (*this)(tuple_types<typename fx_traits::return_type>(), typename fx_traits::args_type(), L);
@@ -131,14 +262,6 @@ struct explicit_lua_func<TFx, T, true> : public lua_func {
     }
 };
 
-
-int lua_cfun(lua_State* L) {
-    void* bridgedata = lua_touserdata(L, lua_upvalueindex(1));
-    auto* fx = static_cast<lua_func*>(bridgedata);
-    int r = fx->operator()(L);
-    return r;
-}
-} // detail
 } // sol
 
 #endif // SOL_LUA_FUNC_HPP
