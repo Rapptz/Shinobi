fb4001512ba6ea01a478e63704f54f728796e964
Rapptz <rapptz@gmail.com>
2014-01-19 15:46:01 -0500

    Add base compiler class

---
+++ src/compiler.hpp
@@ -0,0 +1,24 @@
+#ifndef COMPILER_HPP
+#define COMPILER_HPP
+
+#include <string>
+#include <sol/object.hpp>
+
+class compiler {
+protected:
+    std::string compiler_name;
+public:
+    std::string name() const noexcept {
+        return compiler_name;
+    }
+
+    virtual std::string initial() const = 0;
+    virtual std::string include_path(const sol::object&) const = 0;
+    virtual std::string compiler_flags(const sol::object&) const = 0;
+    virtual std::string output() const = 0;
+    virtual std::string library_path(const sol::object&) const = 0;
+    virtual std::string linker_flags(const sol::object&) const = 0;
+    virtual std::string libraries(const sol::object&) const = 0;
+};
+
+#endif // COMPILER_HPP
