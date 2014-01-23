b0d046be1aec37d521462cfb546568d57f76f8c0
Rapptz <rapptz@gmail.com>
2014-01-23 01:02:07 -0500

    Add MSVC support

---
+++ src/msvc.hpp
@@ -0,0 +1,70 @@
+#ifndef MSVC_HPP
+#define MSVC_HPP
+
+#include "compiler.hpp"
+
+class msvc : public compiler {
+public:
+    msvc(): compiler("cl") {}
+    msvc(std::string): compiler("cl") {}
+
+    virtual std::string initial() {
+        return "/nologo /showIncludes";
+    }
+
+    virtual std::string include_path(const sol::object& obj) {
+        if(obj.is<sol::table>()) {
+            const auto& t = obj.as<sol::table>();
+            to_string(t, "/I\"", "\"");
+            return ss.str();
+        }
+
+        return "";
+    }
+
+    virtual std::string compiler_flags(const sol::object& obj) {
+        if(obj.is<sol::table>()) {
+            const auto& t = obj.as<sol::table>();
+            to_string(t);
+            return ss.str();
+        }
+
+        return "";
+    }
+
+    virtual std::string output() {
+        return "/c /Fo$out $in";
+    }
+
+    virtual std::string library_path(const sol::object& obj) {
+        if(obj.is<sol::table>()) {
+            const auto& t = obj.as<sol::table>();
+            to_string(t, "/libpath:\"", "\"");
+            return ss.str();
+        }
+
+        return "";
+    }
+
+    virtual std::string linker_flags(const sol::object& obj) {
+        if(obj.is<sol::table>()) {
+            const auto& t = obj.as<sol::table>();
+            to_string(t);
+            return ss.str();
+        }
+
+        return "";
+    }
+
+    virtual std::string libraries(const sol::object& obj) {
+        if(obj.is<sol::table>()) {
+            const auto& t = obj.as<sol::table>();
+            to_string(t);
+            return ss.str();
+        }
+
+        return "";
+    }
+};
+
+#endif // MSVC_HPP
